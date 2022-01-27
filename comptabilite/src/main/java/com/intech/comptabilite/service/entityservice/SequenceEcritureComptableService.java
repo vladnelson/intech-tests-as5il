83f21f1b2b674e612f29e5ad101f8019b7263e6c
vladnelson <didimilca@hotmail.fr>
2022-01-27 14:40:18 +0100

    JournalComptableServiceUnitTest

--- comptabilite/src/main/java/com/intech/comptabilite/service/entityservice/SequenceEcritureComptableService.java
+++ comptabilite/src/main/java/com/intech/comptabilite/service/entityservice/SequenceEcritureComptableService.java
@@ -1,6 +1,8 @@
 package com.intech.comptabilite.service.entityservice;
 
 import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,5 +31,6 @@ public class SequenceEcritureComptableService {
 	public void upsert(SequenceEcritureComptable sequenceEcritureComptable) {
 		repository.save(sequenceEcritureComptable);
 	}
+	
 	
 }
